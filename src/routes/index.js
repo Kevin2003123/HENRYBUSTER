85613c2f81a446c8ad1accb4ffc1c5e7836b6ba4
Kevin2003123 <kevinreyes005@gmail.com>
2023-05-12 17:58:05 -0400

    change Languaje for Language because the route dont work

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const getMovies = require("../controllers/getMovies");
 const getFormat = require("../controllers/getFotmat");
 const getGenre = require("../controllers/getGenres");
-const getLanguaje = require("../controllers/getLanguaje");
+const getLanguage = require("../controllers/getLanguage");
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ const router = express.Router();
 router.get("/movies", getMovies);
 router.get("/formats", getFormat);
 router.get("/genres", getGenre);
-router.get("/languajes", getLanguaje);
+router.get("/languages", getLanguage);
 
 
 module.exports = router;
